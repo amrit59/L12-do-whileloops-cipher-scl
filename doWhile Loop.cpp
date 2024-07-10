8e9330c59ce84adb964eb0252a59bbb06c44cfe1
Amrit Raj <121791502+amrit59@users.noreply.github.com>
2024-07-10 23:26:19 +0530

    Add files via upload

---
+++ doWhile Loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int password;
+	
+	do{
+		cin>>password;
+	}
+	while(password<999999)
+	
+	
+	
+	
+/*	cout<<"Enter the password: ";
+	cin>>password;
+	while(password<999999){
+		cout<<"The password does not meet the required condition,please enter the password again"<
+		cin>>password;
+	}
+	cout<<"The user has now entered the correct password"<<endl;*/
+
+	return0;
+}
